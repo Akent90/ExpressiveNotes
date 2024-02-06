37d5a4e40b84079fb87538de62358485880af5e5
akent <alexanderkent099@gmail.com>
2024-02-06 15:42:47 -0500

    Create an API endpoint to retrieve notes from 'db.json'

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET route for notes page
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        return res.status(500).send('Error reading notes');
+    }
+    res.json(JSON.parse(data));
+});
+});
